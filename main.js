4d0a3ca26fa5c464b3c0ae1c69d6e158bacf1573
herbertribeiro19 <hebert11223@gmail.com>
2022-10-11 22:21:48 -0300

    att

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
-'use strict';
+const init = () => {
+    const inputCep = document.querySelector('input[type="cep"]');
+    const submitButton = document.querySelector('#sub');
+    console.log(inputCep, sub);
 
-const pesquisarCep = () => {
-    const cep = document.getElementById('cep').value;
-    const url = `http://viacep.com.br/ws/${cep}/json/`;
-    console.log(cep);
+    if(submitButton){
+        submitButton.addEventListener('click', (event) => {
+            event.preventDefault();
+            fetch('http://viacep.com.br/ws/${cep}/json/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    cep: inputCep.value,
+                })
+            }).then((response) => {
+                return response.json();
+            }).then((data) => {
+                console.log(data)
+            })
+        })
+    }
 }
 
-document.getElementById('cep')
-        .addEventListener('focusout', pesquisarCep);
+window.onload = init;
